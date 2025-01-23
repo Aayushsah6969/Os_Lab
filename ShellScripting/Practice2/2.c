1d43d0f97f9788720b3ce915c0683485ba058bcf
Aayushsah6969 <aayushsah269@gmail.com>
2025-01-23 14:50:52 +0530

    commit-3

--- ShellScripting/Practice2/2.c
+++ ShellScripting/Practice2/2.c
@@ -6,6 +6,6 @@ int main(int argc, char *argv[])
     printf("PID of ex1.c = %d\n",getpid());
     char *args[] = {"Hello", "Neso","Academy", NULL};
     execv("./ex2", args);
-    printf("Back to ex1.c");
+    printf("Back to ex1.c\n");
     return 0;
 }
---
+++ ShellScripting/Practice2/4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+int main() {
+pid_t pid;
+pid = fork();
+if (pid == 0) { // Child process
+printf("Child process with PID: %d\n", getpid());
+exit(0); // Terminate child
+} else if (pid > 0) { // Parent process
+wait(NULL); // Wait for child to finish
+printf("Parent process with PID: %d, child exited with PID: %d\n", getpid(), pid);
+} else {
+perror("fork failed");
+exit(1);
+}
+return 0;
+}
