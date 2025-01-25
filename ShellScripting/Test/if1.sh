be78d6cbbb063ffc99082a5faf3fc6695a7404ff
Aayushsah6969 <aayushsah269@gmail.com>
2025-01-25 15:08:40 +0530

    deleted test files

--- ShellScripting/Test/if1.sh
+++
@@ -1,8 +0,0 @@
-echo "Enter a number "
-read num
-if [ $num -gt 10 ]
-then
-echo "Number is greater than 10"
-else
-echo "Number is lesser than 10"
-fi
--- ShellScripting/Test/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "enter a number"
-read num
-if [ "$num" % 2 == 0 ]
-then
-echo "Even number"
-else
-echo "Odd number"
-fi
