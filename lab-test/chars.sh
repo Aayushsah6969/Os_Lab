410f30bac09ab82b7f98cbd02a00b8468c411b2f
Aayushsah6969 <aayushsah269@gmail.com>
2025-02-06 15:19:28 +0530

    asdfg

--- lab-test/chars.sh
+++ lab-test/chars.sh
@@ -3,22 +3,22 @@
 echo "Enter a string"
 read str
 
-len=${#str}  # Get the length of the string
+len=${#str}  
 
 if (( len % 2 == 0 )); then
     for (( i = 1; i <= len; i++ )); do
         for (( j = len; j > i; j-- )); do
-            echo -n " "  # Print leading spaces
+            echo -n " "  
         done
         for (( j = 1; j <= i; j++ )); do
-            echo -n "* "  # Print stars with spaces
+            echo -n "* "  
         done
-        echo  # Move to the next line
+        echo  
     done
 else
     for (( i = 1; i <= len; i++ )); do
         for (( j = 1; j <= i; j++ )); do
-            echo -n "$i "  # Print number pattern with spaces
+            echo -n "$i "  
         done
         echo
     done
